b24928a0db848ff7a8d4a9bd8ae364da173897cf
Preshen Govender <preshen4@gmail.com>
2023-11-18 17:41:50 +0200

    Added Models

---
+++ Models/CallNumberModel.cs
@@ -0,0 +1,14 @@
+﻿namespace NovelNestLibraryAPI.Models
+{
+    public class CallNumberModel
+    {
+        public int CallNumber { get; set; }
+        public string Description { get; set; }
+
+        public CallNumberModel(int callnumber, string description)
+        {
+            CallNumber = callnumber;
+            Description = description;
+        }
+    }
+}
